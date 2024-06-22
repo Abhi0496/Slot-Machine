698d3617cce92f15870f249846c41ebedcffd66c
Abhi0496 <aaagrawal.abhishek@gmail.com>
2024-06-22 01:48:29 -0700

    Added code for getting number of lines

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+MAX_LINES = 3
+
 def deposit():
     while True:
         amount = input("Enter money you want to deposit: $")
@@ -11,9 +13,30 @@ def deposit():
             
         else:
             print("Enter digits only")
+    
+    return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input(f"Enter the number of lines to bet on (1 - {MAX_LINES})? ")
+
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print(f"Lines must be between 1 - {MAX_LINES}")
+            
+        else:
+            print("Enter digits only")
+    
+    return lines
+
 
 def main():
     balance = deposit()
+    lines = get_number_of_lines()
+    print(balance,lines)
 
 if __name__ == "__main__":
     main()
